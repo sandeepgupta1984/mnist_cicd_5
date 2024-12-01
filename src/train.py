b3594ddf1fb1f9b24ffba79b3d9cc1ad2fea2050
sandeep gupta <sandeep.gupta@walmart.com>
2024-12-01 15:31:50 +0530

    Fix too many parameters issue - 3rd time

--- src/train.py
+++ src/train.py
@@ -7,17 +7,17 @@ import datetime
 class MNIST_CNN(nn.Module):
     def __init__(self):
         super(MNIST_CNN, self).__init__()
-        self.conv1 = nn.Conv2d(1, 8, kernel_size=3, padding=1)
-        self.conv2 = nn.Conv2d(8, 16, kernel_size=3, padding=1)
+        self.conv1 = nn.Conv2d(1, 6, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(6, 12, kernel_size=3, padding=1)
         self.pool = nn.MaxPool2d(2)
-        self.fc1 = nn.Linear(16 * 7 * 7, 32)
-        self.fc2 = nn.Linear(32, 10)
+        self.fc1 = nn.Linear(12 * 7 * 7, 24)
+        self.fc2 = nn.Linear(24, 10)
         self.relu = nn.ReLU()
 
     def forward(self, x):
         x = self.pool(self.relu(self.conv1(x)))  # 28x28 -> 14x14
         x = self.pool(self.relu(self.conv2(x)))  # 14x14 -> 7x7
-        x = x.view(-1, 16 * 7 * 7)
+        x = x.view(-1, 12 * 7 * 7)
         x = self.relu(self.fc1(x))
         x = self.fc2(x)
         return x
